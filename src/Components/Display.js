6a2b15362e73361850d8142b2d5c327e7d238596
sai1781 <96867450+sai1781@users.noreply.github.com>
2022-10-29 19:13:52 +0530

    Add files via upload

---
+++ src/Components/Display.js
@@ -0,0 +1,25 @@
+
+
+const Display = ({ colors }) => {
+    console.log(colors);
+    const number = 12355;
+    return (
+        <>
+        <h2>Colors & ColorCode</h2>
+        <hr />
+
+            {colors.map((e, idx) => {
+                return (
+                    <div className = "displays" style={{ height:135, width:110}}>
+                        <p style={{height:100, width:110, backgroundColor:`${e.color_code}`}}> </p>
+                        <p className="text" key={idx} >{e.color_code}</p>
+                        <span className="text"  style={{color:`${e.color_code}`, fontSize:10}} >{e.color_name}</span>
+
+                    </div>
+                )
+            })}
+        </>
+    )
+}
+
+export default Display;
